9c3b5dcd2f154ba0d19f882c3867ab28056fc3ff
jago-1009 <garwoodj22@gmail.com>
2023-10-03 13:23:50 -0400

    Compression.

--- homework4/arraysAssignment.js
+++ homework4/arraysAssignment.js
@@ -1,56 +1,43 @@
 const students = [
-
  { name: 'Alice', grades: [85, 92, 78, 90] },
-
  { name: 'Bob', grades: [75, 88, 95, 100] },
-
  { name: 'Charlie', grades: [60, 72, 68, 74] },
-
  { name: 'David', grades: [92, 88, 95, 98] },
-
  { name: 'Eve', grades: [100, 100, 100, 100] }
-
  ];
-
 let pArray = document.querySelectorAll('p')
 console.log(pArray)
-students.forEach((name) => 
-pArray[0].innerHTML += "<br>" + (name.name));
-
-let result1 = students.every(student => student.grades.every(grade => grade >= 60));
-pArray[1].innerHTML += result1
-let result2 = students.some(student => student.grades.some(grade => grade == 100));
-pArray[2].innerHTML += result2
-let result3= students.filter(passedClass)
-pArray[3].innerHTML += "<br>" + result3[0].name
-pArray[3].innerHTML += "<br>" + result3[1].name
-let result4 = students.map(FindAverage)
+//Question 1
+students.forEach((student) => 
+pArray[0].innerHTML += "<br>" + (student.name));
+//Question 2
+let Question2 = students.every(student => student.grades.every(grade => grade >= 60));
+pArray[1].innerHTML += Question2
+//Question 3
+let Question3 = students.some(student => student.grades.some(grade => grade == 100));
+pArray[2].innerHTML += Question3
+//Question 4
+let Question4= students.filter(function(studentAVG) {
+    if (FindAverage(studentAVG) >= 90) {
+        return studentAVG
+    }
+})
+pArray[3].innerHTML += "<br>" + Question4[0].name
+pArray[3].innerHTML += "<br>" + Question4[1].name
+//Question 5
+let Question5 = students.map(FindAverage)
 for(i=0;i<5;i++) {
-    pArray[4].innerHTML += "<br>" + students[i].name + "'s Average is :" + result4[i]
+    pArray[4].innerHTML += "<br>" + students[i].name + "'s Average is :" + Question5[i]
 }
-
-
+//Question 6
 for(x=0;x<5;x++) {
-let totalGrade = students[x]['grades'].reduce((total, grade) => total + grade, 0)
- pArray[5].innerHTML += "<br>" + students[x].name + "'s Average is :" + totalGrade
+    let totalGrade = students[x]['grades'].reduce((total, grade) => total + grade, 0)
+    pArray[5].innerHTML += "<br>" + students[x].name + "'s Average is :" + totalGrade
 }
-
-function passedClass(e) {
+//Functions
+function FindAverage(e) {
     let total
     total = e.grades[0] + e.grades[1] + e.grades[2] + e.grades[3] 
     average = total/4
-
-    return average >=90
+ return average 
 }
-function FindAverage(e) {
-      let total
-    total = e.grades[0] + e.grades[1] + e.grades[2] + e.grades[3] 
-    average = total/4
- return average
-}
-
-
-    
-
-
-
