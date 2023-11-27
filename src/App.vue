bc0e5acbd83e0a3d85bcceeef8fb63f73dd06feb
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2023-11-27 08:57:42 -0500

    SRC Upload, Homework 7

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<script >
+export default {
+  data() {
+    return {
+      teamName: '',
+      roleFilter: ''
+    }
+  },
+  methods: {
+    setTeamDetails(data) {
+       this.teamName = data.teamName
+      this.roleFilter = data.roleSelected
+      console.log("TEAM", this.teamName, "ON", this.roleFilter)
+      this.$emit('filter-role', {
+        filterRole: data.roleSelected
+      })
+    }
+  }
+}
+
+</script>
+
+<template>
+
+
+  <TextForm @form-submit="setTeamDetails" @change-text="textChange"></TextForm>
+  <DataList   :team-name="teamName" :role-selected="roleFilter"></DataList>
+
+
+</template>
+
+<style>
+template {
+  display: block;
+}
+</style>
